308ce89a7c674944b95aa446cc4bc994b88b9e4c
navarrs <ingrid.navarro.an@gmail.com>
2021-04-26 12:21:03 -0600

    started q3.2.2

--- hw4/code/rl.py
+++ hw4/code/rl.py
@@ -6,7 +6,8 @@
 
 import numpy as np
 import gridworld
-
+import matplotlib.pyplot as plt
+import math
 
 def value_iteration(env, gamma, max_iterations=int(1e3), tol=1e-3):
   """
@@ -32,45 +33,40 @@ def value_iteration(env, gamma, max_iterations=int(1e3), tol=1e-3):
   """
   A = env.nA
   S = env.nS
+  P = env.P
+  n_iter = 0
+  delta = np.finfo(np.float32).max
   
-  def maxs(v1, v2):
-    return np.max(np.abs(v1 - v2))
-  
-  def updateQ(Q, V, s):
+  def updateV(V, s):
+    max_v = np.finfo(np.float32).min
+    
     for a in range(A):
-      cs, r, done, P = env.step(a)
-      T = env.generateTransitionMatrices()
       v = 0.0
-      for ns in range(s, S):
-        v += T[s][a][ns] * V[ns]
-      Q[s, a] = r + gamma * v
-    return Q
-  
-  niter = 0
+      # P[state][action] = (prob, state, reward, done)
+      transition = P[s][a]
+      for p, ns, r, done in transition:
+        v += p * (r + gamma * V[ns])   
+      max_v = np.maximum(max_v, v)
 
-  # initialize the value functions
-  Vt = np.random.rand(S)
-  Vs = np.random.rand(S)
+    V[s] = max_v
+    return V
   
-  Q = np.zeros(shape=(S, A), dtype=np.float32)
-  
-  while maxs(Vs, Vt) >= tol and niter < max_iterations:
-    Vt = np.copy(Vs)
+  V = np.random.rand(S)
+  while n_iter < max_iterations and delta > tol:
+    delta = 0.0
     
-    # go over all states
+    # loop over all states
     for s in range(S):
-      Q = updateQ(Q, Vt, s)
-      pi = np.argmax(Q[s, :])
-      Vs[s] = Q[s, pi]
+      v = V[s]
+      V = updateV(V, s)
+      delta = np.maximum(delta, np.abs(v - V[s]))
     
-    if (niter+1) % 10 == 0:
-      print(f"\nstep: [{niter+1}/{max_iterations}]")
-      print(f"\t-- delta: {maxs(Vs, Vt)}")
-      # print(f"\t-- value: {Vs}")
-      
-    niter += 1
-      
-  return Vs, niter
+    n_iter += 1
+    if n_iter % 10 == 0:
+      print(f"step: [{n_iter}/{max_iterations}]")
+      print(f"\t--delta: {delta}")
+  
+  return V, n_iter
 
 
 def policy_from_value_function(env, value_function, gamma):
@@ -94,13 +90,32 @@ def policy_from_value_function(env, value_function, gamma):
       policy: Array of integers where each element is the optimal action to take
         from the state corresponding to that index.
     """
-
-    ## YOUR CODE HERE ##
-    raise NotImplementedError()
+    A = env.nA
+    S = env.nS
+    
+    def Qat(s, a):
+      cs, r, done, P = env.step(a)
+      T = env.generateTransitionMatrices()
+      v = 0.0
+      for ns in range(s, S):
+        v += T[s, a, ns] * (r + gamma * value_function[ns])
+      return v
+      
+    policy = np.zeros(shape=(S, 1), dtype=np.float32)
+    
+    for s in range(S):
+      q = np.zeros(shape=(4, 1), dtype=np.float32)
+      
+      for a in range(A):
+        q[a] = Qat(s, a)
+        
+      policy[s] = np.argmax(q)
+      
+    return policy
 
 
 def policy_iteration(env, gamma, max_iterations=int(1e3), tol=1e-3):
-    """
+  """
     Q3.2.2: BONUS
     This implements policy iteration for learning a policy given an environment.
 
@@ -123,11 +138,74 @@ def policy_iteration(env, gamma, max_iterations=int(1e3), tol=1e-3):
       (numpy.ndarray, iteration)
       value_function:  Optimal value function
       iteration: number of iterations it took to converge.
-    """
+  """
+  A = env.nA
+  S = env.nS
+  P = env.P
+  n_iter = 0
+  policy_stable = False
+  
+  def evaluate_policy(n_iter):
+    V = np.random.rand(S)
+    delta = np.finfo(np.float32).max
+    while n_iter < max_iterations and delta > tol:
+      delta = 0.0
+      
+      # loop over all states
+      for s in range(S):
+        v = V[s]
+        max_v = np.finfo(np.float32).min
+        
+        # loop over all actions 
+        for a in range(A):
+          vc = 0.0
+          
+          # P[state][action] = (prob, state, reward, done)
+          transition = P[s][a]
+          for p, ns, r, done in transition:
+            vc += p * (r + gamma * V[ns])   
+            
+          # find action that maximizes value 
+          max_v = np.maximum(max_v, vc)
 
-    ## BONUS QUESTION ##
-    ## YOUR CODE HERE ##
-    raise NotImplementedError()
+        V[s] = max_v
+        delta = np.maximum(delta, np.abs(v - V[s]))
+        n_iter += 1
+        
+        if n_iter % 10 == 0:
+          print(f"step: [{n_iter}/{max_iterations}]")
+          print(f"\t--delta: {delta}")
+                  
+    return V, n_iter
+  
+  def improve_policy(V, policy):
+    policy_stable = True
+    for s in range(S):
+      old_action = policy[s]
+      max_v = np.finfo(np.float32).min
+      max_action = 0
+      for a in range(A):
+        v = 0.0
+        # P[state][action] = (prob, state, reward, done)
+        transition = P[s][a]
+        for p, ns, r, done in transition:
+          v += p * (r + gamma * V[ns]) 
+        
+        max_action = a if max_v < v else max_action
+      
+      policy[s] = max_action
+      if not policy[s] == old_action:
+        policy_stable = False 
+        
+    return policy, policy_stable
+
+  policy = np.zeros(shape=(S, 1), dtype=np.int)
+  
+  while not policy_stable and n_iter < max_iterations:
+    V, n_iter = evaluate_policy(n_iter)
+    ppolicy, policy_stable = improve_policy(V, policy)
+    
+  return V, n_iter
 
 
 def td_zero(env, gamma, policy, alpha):
@@ -184,26 +262,56 @@ def n_step_td(env, gamma, policy, alpha, n):
     ## YOUR CODE HERE ##
     raise NotImplementedError()
 
+def plot(X, title, text=None, dec=4):
+  ax = plt.gca()
+  im = ax.imshow(X)
+  cbar = ax.figure.colorbar(im, ax=ax)
+  ax.set_title(title)
+  
+  for i in range(X.shape[0]):
+    for j in range(X.shape[1]):
+      if not text is None:
+        v = text[int(X[i, j])]
+      else:
+        v = X[i, j]
+        factor = 10.0 * dec
+        v = math.trunc(v * factor) / factor
+      ax.text(j, i, v, ha="center", va="center", color="w")
+  
+  plt.savefig(f"{title}.png")
+  # plt.show()
+  plt.close()
 
 if __name__ == "__main__":
-    env = gridworld.GridWorld(map_name='8x8')
+  mapname = "8x8"
+  env = gridworld.GridWorld(map_name=mapname)
+  gw, gh = int(mapname[0]), int(mapname[-1])
 
-    # Play around with these values if you want!
-    gamma = 0.9
-    alpha = 0.05
-    n = 10
+  # Play around with these values if you want!
+  gamma = 0.9
+  alpha = 0.05
+  n = 10
+  action_names = ['L', 'D', 'R', 'U']
+  
+  # Q3.2.1
+  print(f"\n** q3.2.1 value iteration")
+  V_vi, n_iter = value_iteration(env, gamma)
+  plot(V_vi.reshape(gw, gh), 
+       title='value_iteration')
+  print(f"value iteration converged after {n_iter} steps")
+  policy = policy_from_value_function(env, V_vi, gamma)
+  plot(policy.reshape(gw, gh), 
+       title='policy_from_value_iteration', 
+       text=action_names)
 
-    # Q3.2.1
-    V_vi, n_iter = value_iteration(env, gamma)
-    # print(f"value iteration converged after {n_iter} steps with value:\n{V_vi}")
-    print(f"value iteration converged after {n_iter} steps")
-    # policy = policy_from_value_function(env, Vs, gamma)
 
-    # Q3.2.2: BONUS
-    # V_pi, n_iter = policy_iteration(env, gamma)
+  # Q3.2.2: BONUS
+  print(f"\n** q3.2.2 policy iteration")
+  V_pi, n_iter = policy_iteration(env, gamma)
+  print(f"policy iteration converged after {n_iter} steps")
 
-    # Q3.2.3
-    # V_td = td_zero(env, gamma, policy, alpha)
+  # Q3.2.3
+  # V_td = td_zero(env, gamma, policy, alpha)
 
-    # Q3.2.4: BONUS
-    # V_ntd = n_step_td(env, gamma, policy, alpha, n)
+  # Q3.2.4: BONUS
+  # V_ntd = n_step_td(env, gamma, policy, alpha, n)
